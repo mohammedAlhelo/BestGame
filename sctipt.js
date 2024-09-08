62fcc1ff28f80c1d51510571d651ae8c7335131d
mohammedalhelo <mohammed.alhelo@outlook.com>
2024-09-08 22:28:21 +0300

    java script progress

--- sctipt.js
+++ sctipt.js
@@ -9,30 +9,76 @@ let redIntial = 0
 let redRemove = 0
 let greenIntial = 0
 let greenRemove = 0
-let x = Math.ceil(Math.random() * 6)
 
 // game logic
 for (let i = 0; i < block.length; i++) {
   if (player == 1) {
-    if (redIntial < block.length) {
+    if (redIntial < 26) {
       // random number
       button1.onclick = () => {
+        let x = Math.ceil(Math.random() * 6)
+        player = 2
+        currentPlayer.innerHTML = player
         diceValue.innerHTML = x
-        if (redIntial + x < block.length) {
+        if (redIntial + x < 26) {
           // selecting the block and changing the color
           redIntial = x + redIntial
-          redRemove = redIntial - x
+          redRemove = Math.abs(redIntial - x)
           let addRed = document.getElementById(redIntial)
           let removeRed = document.getElementById(redRemove)
           addRed.classList.add('red')
           removeRed.classList.add('white')
           // winning condition
-          if (redIntial == block.length) {
+          if (redIntial == 25) {
             alert('player one won!!')
           }
         }
-        player = 2
+      }
+    }
+  }
+  // if (greenIntial < 26) {
+  //   // random number
+  //   button2.onclick = () => {
+  //     let y = Math.ceil(Math.random() * 6)
+  //     diceValue.innerHTML = y
+  //     if (greenIntial + y < 26) {
+  //       // selecting the block and changing the color
+  //       greenIntial = y + greenIntial
+  //       greenRemove = Math.absgreenIntial - y
+  //       let addgreen = document.getElementById(greenIntial)
+  //       let removeGreen = document.getElementById(greenRemove)
+  //       addgreen.classList.add('green')
+  //       removeGreen.classList.add('white')
+  //       // winning condition
+  //       if (greenIntial == 25) {
+  //         alert('player two won!!')
+  //       }
+  //     }
+  //     player = 1
+  //     currentPlayer.innerHTML = player
+  //   }
+  // }
+  else {
+    if (greenIntial < 26) {
+      // random number
+      button2.onclick = () => {
+        let x = Math.ceil(Math.random() * 6)
+        player = 1
         currentPlayer.innerHTML = player
+        diceValue.innerHTML = x
+        if (greenIntial + x < 26) {
+          // selecting the block and changing the color
+          greenIntial = x + greenIntial
+          greenRemove = Math.abs(greenIntial - x)
+          let addGreen = document.getElementById(greenIntial)
+          let removeGreen = document.getElementById(redRemove)
+          addGreen.classList.add('green')
+          removeGreen.classList.add('white')
+          // winning condition
+          if (greenIntial == 25) {
+            alert('player one won!!')
+          }
+        }
       }
     }
   }

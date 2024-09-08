e5fa9f89397134e9804595617be3496bfaa2a7e0
mohammedalhelo <mohammed.alhelo@outlook.com>
2024-09-08 12:38:29 +0300

    java script player one done

--- sctipt.js
+++ sctipt.js
@@ -6,6 +6,10 @@ let button1 = document.querySelectorAll('button')[0]
 let button2 = document.querySelectorAll('button')[1]
 let diceValue = document.querySelector('p')
 let redIntial = 0
+let redRemove = 0
+let greenIntial = 0
+let greenRemove = 0
+let x = Math.ceil(Math.random() * 6)
 
 // game logic
 for (let i = 0; i < block.length; i++) {
@@ -13,22 +17,23 @@ for (let i = 0; i < block.length; i++) {
     if (redIntial < block.length) {
       // random number
       button1.onclick = () => {
-        for (let i = 0; i < 25; i++) {
-          let x = Math.ceil(Math.random() * 6)
-          diceValue.innerHTML =
-            // selecting the block and changing the color
-
-            redIntial = x + redIntial
-          let k = document.getElementById(redIntial)
-          k.classList.add('red')
-          i = redIntial
+        diceValue.innerHTML = x
+        if (redIntial + x < block.length) {
+          // selecting the block and changing the color
+          redIntial = x + redIntial
+          redRemove = redIntial - x
+          let addRed = document.getElementById(redIntial)
+          let removeRed = document.getElementById(redRemove)
+          addRed.classList.add('red')
+          removeRed.classList.add('white')
           // winning condition
           if (redIntial == block.length) {
             alert('player one won!!')
           }
         }
+        player = 2
+        currentPlayer.innerHTML = player
       }
-    } else {
     }
   }
 }
